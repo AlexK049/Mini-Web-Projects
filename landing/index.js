513b81e3e06b84404ad218581cfc95f5c82ffb30
alex <akadlofa@gmail.com>
2024-01-19 17:54:41 -0500

    made arrows only appear if the user can scroll in that direction

--- landing/index.js
+++ landing/index.js
@@ -5,26 +5,28 @@ let activeCardIndex = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
     //this will set the zindex of all the cards properly
-    setActiveCard("left");
+    updateCardStyles();
+    updateScrollBtnStyles();
 })
 
 function scrollContent(direction) {
     const scrollableContent = document.getElementById("carousel-cards");
     if (direction === "left") {
         scrollableContent.scrollLeft -= SCROLL_AMOUNT;
+        if (activeCardIndex > 0) {
+            activeCardIndex--;
+        }
     } else if (direction === "right") {
         scrollableContent.scrollLeft += SCROLL_AMOUNT;
+        if (activeCardIndex < NUM_CARDS - 1) {
+            activeCardIndex++;
+        }
     }
-    setActiveCard(direction);
+    updateCardStyles();
+    updateScrollBtnStyles();
 }
 
-function setActiveCard(direction) {
-    if (direction === "left" && activeCardIndex > 0) {
-        activeCardIndex--;
-    } else if (direction === "right" && activeCardIndex < NUM_CARDS - 1) {
-        activeCardIndex++;
-    }
-
+function updateCardStyles() {
     document.querySelectorAll(".card").forEach((card, i) => {
         //set the zIndex of the cards to have the active card be on top
         card.style.position = "relative";
@@ -33,4 +35,15 @@ function setActiveCard(direction) {
         //scale the cards to make the active one the biggest, and all the rest slightly smaller
         card.style.transform = `scale(${1 - (i !== activeCardIndex ? 0.1 : 0)})`
     });
+}
+
+function updateScrollBtnStyles() {
+    if (activeCardIndex >= NUM_CARDS - 1) {
+        document.getElementById("scroll-cards-right").style.display = "none";
+    } else if (activeCardIndex <= 0) {
+        document.getElementById("scroll-cards-left").style.display = "none";
+    } else {
+        document.getElementById("scroll-cards-right").style.display = "";
+        document.getElementById("scroll-cards-left").style.display = "";
+    }
 }
