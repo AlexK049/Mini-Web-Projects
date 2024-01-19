6ea7e35dfb88fa360468922ef20c1c9139b58234
alex <akadlofa@gmail.com>
2024-01-19 17:06:09 -0500

    carousel functionality complete

--- landing/index.js
+++ landing/index.js
@@ -1,10 +1,36 @@
+const SCROLL_AMOUNT = 550;
+const NUM_CARDS = 6;
+
+let activeCardIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    //this will set the zindex of all the cards properly
+    setActiveCard("left");
+})
+
 function scrollContent(direction) {
     const scrollableContent = document.getElementById("carousel-cards");
-    const scrollAmount = 200;
-
     if (direction === "left") {
-        scrollableContent.scrollLeft -= scrollAmount;
+        scrollableContent.scrollLeft -= SCROLL_AMOUNT;
     } else if (direction === "right") {
-        scrollableContent.scrollLeft += scrollAmount;
+        scrollableContent.scrollLeft += SCROLL_AMOUNT;
     }
+    setActiveCard(direction);
+}
+
+function setActiveCard(direction) {
+    if (direction === "left" && activeCardIndex > 0) {
+        activeCardIndex--;
+    } else if (direction === "right" && activeCardIndex < NUM_CARDS - 1) {
+        activeCardIndex++;
+    }
+
+    document.querySelectorAll(".card").forEach((card, i) => {
+        //set the zIndex of the cards to have the active card be on top
+        card.style.position = "relative";
+        card.style.zIndex = `${NUM_CARDS - Math.abs(activeCardIndex - i)}`;
+
+        //scale the cards to make the active one the biggest, and all the rest slightly smaller
+        card.style.transform = `scale(${1 - (i !== activeCardIndex ? 0.1 : 0)})`
+    });
 }
